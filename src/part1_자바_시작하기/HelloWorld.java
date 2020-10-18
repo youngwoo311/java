82c144c3c0ef604fde5c370ca5b96ceef4bb967c
youngwoo311 <devlee311@gmail.com>
2020-10-18 19:22:53 +0900

    refactor

---
+++ src/part1_자바_시작하기/HelloWorld.java
@@ -0,0 +1,34 @@
+package part1_자바_시작하기;
+
+/**
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World1");
+//		System.out.println("Hello World2");
+//		System.out.println("Hello World3");
+//		System.out.println("Hello World4");
+//		System.out.println("Hello World5");
+//		System.out.println("Hello World6");
+//		System.out.println("Hello World7");
+//		System.out.println("Hello World8");
+//		System.out.println("Hello World9");
+
+		int count;
+
+		count = 10;
+
+		System.out.println(count);
+
+		double avg = 11.1;
+		System.out.println(avg);
+
+		String name = "carami";
+		System.out.println(name);
+
+
+	}
+
+}
