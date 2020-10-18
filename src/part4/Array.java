ee1722d3b48659656fc8600732a5a8f9d9ade3bc
youngwoo311 <devlee311@gmail.com>
2020-10-18 19:19:44 +0900

    part4 배열

---
+++ src/part4/Array.java
@@ -0,0 +1,23 @@
+package part4;
+
+import java.awt.*;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array1 = new int[100];
+
+        array1[0] = 50;
+        array1[10] = 100;
+
+        int[] array2 = new int[]{1,2,3,4};
+        int[] array3 = {1,2,3,4};
+
+        System.out.println(array3[3]);
+        int value = array3[0];
+        System.out.println(value);
+
+        for(int i=0; i<4; i++) {
+            System.out.println(array2[i]);
+        }
+    }
+}
---
+++ src/part4/TwoDimensionalArray.java
@@ -0,0 +1,16 @@
+package part4;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        int[][] array = new int[3][4];
+        array[0][0] = 10;
+
+        int[][] array2 = new int[3][];
+        array2[0] = new int[1];
+        array2[0][0] = 10;
+
+        int[][] array3 = {{1}, {1,2}, {1,2,3}};
+        System.out.println(array3[0][0]);
+        System.out.println(array3[2][2]);
+    }
+}
