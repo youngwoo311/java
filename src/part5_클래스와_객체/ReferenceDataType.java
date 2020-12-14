70bb0c3be525eca205e58e3c67e6ec3bbe20280c
youngwoo311 <devlee311@gmail.com>
2020-12-14 23:15:05 +0900

    클래스와객체

---
+++ src/part5_클래스와_객체/ReferenceDataType.java
@@ -0,0 +1,12 @@
+package part5_클래스와_객체;
+
+public class ReferenceDataType {
+
+    public static void main(String[] args) {
+        int i = 4;
+        String str = new String("hello");
+        System.out.println(str);
+
+    }
+
+}
